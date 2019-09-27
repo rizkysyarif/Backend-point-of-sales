19bee5b843967615a91b4fd51b6d7c2959098269
rizkysyarif <55020981+rizkysyarif@users.noreply.github.com>
2019-09-27 21:20:43 +0700

    add users

--- RESTFUL API/src/index.js
+++ RESTFUL API/src/index.js
@@ -4,9 +4,11 @@ const Route     = express.Router()
 //import routes
 const products = require('./routes/products')
 const categories = require('./routes/categories')
+const users = require('./routes/users')
 
 Route
     .use('/product', products)
     .use('/category', categories)
+    .use('/user', users)
 
 module.exports = Route
