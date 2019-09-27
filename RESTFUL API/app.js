de64ada31b63ee521d86a108dd6f6659db3f6d84
rizkysyarif <55020981+rizkysyarif@users.noreply.github.com>
2019-09-27 21:17:54 +0700

    Update app.js
    remove unnecessary comment

--- RESTFUL API/app.js
+++ RESTFUL API/app.js
@@ -1,33 +1,26 @@
-//Impor express versi es5
+//Impor 
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const fileupload = require('express-fileupload')
 const cors = require('cors')
-//const logger   = require('morgan')
+require('dotenv/config')
 
-//Import body-parser
-//const bodyParser = require('body-parser');
 
-//Impor express versi es6
-//import expres from 'express'
+
 const app = express()
+app.use(fileupload())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(fileupload())
 app.use(cors({
     origin: 'localhost'
 }))
 
-//app.use(logger('dev'))
-
-
-//app.use(express.urlencoded({extended: true}))
-//app.use(express.json())
 const routerNav = require('./src/index')
-//Menggunakan bodyparser
 
 
-//mendefinisikan port yang akan digunakan : 5000
+
+
 const port = process.env.SERVER_PORT || 5000
 
 
@@ -38,7 +31,7 @@ app.listen(port, function(){
 app.use('/api', routerNav)
 
 
-// menambahkan 404 Not Found
+
 app.get('*',(req,res)=>{
     res.send('404 Not Found')
 })
